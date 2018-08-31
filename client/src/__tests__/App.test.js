d6531fab280bdd639d603f10ba27b9fdbf89222b
Matteo Gioioso <gioioso.matteo@gmail.com>
2018-08-31 08:08:02 +0300

    Fixed App.js test

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -3,14 +3,15 @@ import { App, AppConnected } from '../App';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store'
 
+const initialState = { auth: { isAuthorized: true } }
+
 it('renders without crashing', () => {
-  const Component = shallow(<App />);
+  const Component = shallow(<App auth={initialState}/>);
 
   expect(Component).toBeDefined();
 });
 
-it('should find the defualt state', () => {
-  const initialState = { auth: { isAuthorized: true } }
+it('should find the defualt state', () => { 
   const mockStore = configureStore();
   const store = mockStore(initialState)
   const Component = shallow(<AppConnected store={store} />);
